8c5f10413d0712492b0c12f811e6433392d0feec
ProgOleg <olegbikov1212@gmail.com>
2021-02-04 02:19:35 +0200

    try resolve migrations problem

--- main_project/settings.py
+++ main_project/settings.py
@@ -11,11 +11,11 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-from dotenv import load_dotenv
+from dotenv import load_dotenv, find_dotenv
 from pathlib import Path  # Python 3.6+ only
 env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
-
+load_dotenv(find_dotenv())
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
